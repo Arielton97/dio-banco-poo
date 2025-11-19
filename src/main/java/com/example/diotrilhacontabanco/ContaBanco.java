1b6bee60207adf6c65547fac55c5b1e3a6569949
Arielton97 <arieltonribeiro97@gmail.com>
2025-11-18 23:44:23 -0300

    First Commit

--- src/main/java/com/example/diotrilhacontabanco/ContaBanco.java
+++ src/main/java/com/example/diotrilhacontabanco/ContaBanco.java
@@ -1,4 +1,23 @@
 package com.example.diotrilhacontabanco;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.beans.ConstructorProperties;
+
 public class ContaBanco {
+
+    @Getter
+    int conta;
+
+    @Getter
+    @Setter
+    double saldo;
+
+    @Getter
+    @Setter
+    String titular;
+
+    @ConstructorProperties( { "conta", "saldo", "titular" } )
+    public ContaBanco() {}
 }
---
+++ src/main/java/com/example/diotrilhacontabanco/Controller.java
@@ -0,0 +1,5 @@
+package com.example.diotrilhacontabanco;
+
+public class Controller {
+
+}
