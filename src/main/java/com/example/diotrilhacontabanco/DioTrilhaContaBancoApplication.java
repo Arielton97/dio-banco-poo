8a0aea72ff6d0e1e8ecda54ff2932729e192c9d4
Arielton97 <arieltonribeiro97@gmail.com>
2025-11-18 23:27:25 -0300

    First Commit

---
+++ src/main/java/com/example/diotrilhacontabanco/DioTrilhaContaBancoApplication.java
@@ -0,0 +1,13 @@
+package com.example.diotrilhacontabanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioTrilhaContaBancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioTrilhaContaBancoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diotrilhacontabanco/DioTrilhaContaBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diotrilhacontabanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioTrilhaContaBancoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
